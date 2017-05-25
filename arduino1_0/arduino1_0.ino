10166a0f8b7b26465974097a8250cc87392a3d09
Nathan Pugsley <pugsleyn@uni.coventry.ac.uk>
2017-05-25 15:07:27 +0100

    Sends arrays of data from processing to arduino, mouse selects LED and colour

--- arduino1_0/arduino1_0.ino
+++ arduino1_0/arduino1_0.ino
@@ -6,8 +6,11 @@ char val;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(144, PIN, NEO_GRB + NEO_KHZ800);
 
+int currentValue = 0;
+int values[] = {0,0};
+
 void setup() {
-  Serial.begin(9200);
+  Serial.begin(9600);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
   strip.setBrightness(32);
@@ -16,96 +19,18 @@ void setup() {
 void loop() {
   if (Serial.available()) 
      { // If data is available to read,
-       val = Serial.read(); // read it and store it in val
+       int incomingValue = Serial.read(); // read it and store it
+       
+       values[currentValue] = incomingValue;
+        
+       currentValue++;
+       if(currentValue > 1){
+         currentValue=0;
+       }
+       
+       strip.setPixelColor(values[0], values[1], values[0], values[1]);
+       strip.show();
      }
-       if (val == '1') 
-       { // If 1 was received
-          strip.setPixelColor(132, 255, 0, 0);
-          strip.show();
-           
-       } if (val != '1') {
-          strip.setPixelColor(132, 0, 0, 0);
-          strip.show();
-       }
-       
-       if (val == '2') {
-          strip.setPixelColor(133, 255, 0, 0);
-          strip.show();
-       }
-
-       if (val != '2') {
-          strip.setPixelColor(133, 0, 0, 0);
-          strip.show();
-       }
-           
-       if (val == '3') {
-          strip.setPixelColor(134, 255, 0, 0);
-          strip.show();
-       }
-
-       if (val != '3') {
-          strip.setPixelColor(134, 0, 0, 0);
-          strip.show();
-       }
-
-        if (val == '4') {
-          strip.setPixelColor(135, 255, 0, 0);
-          strip.show();
-       }
-
-       if (val != '4') {
-          strip.setPixelColor(135, 0, 0, 0);
-          strip.show();
-       }
-        if (val == '5') 
-       { // If 1 was received
-          strip.setPixelColor(136, 255, 0, 0);
-          strip.show();
-           
-       } if (val != '5') {
-          strip.setPixelColor(136, 0, 0, 0);
-          strip.show();
-       }
-       
-       if (val == '6') {
-          strip.setPixelColor(137, 255, 0, 0);
-          strip.show();
-       }
-
-       if (val != '6') {
-          strip.setPixelColor(137, 0, 0, 0);
-          strip.show();
-       }
-           
-       if (val == '7') {
-          strip.setPixelColor(138, 255, 0, 0);
-          strip.show();
-       }
-
-       if (val != '7') {
-          strip.setPixelColor(138, 0, 0, 0);
-          strip.show();
-       }
-
-        if (val == '8') {
-          strip.setPixelColor(139, 255, 0, 0);
-          strip.show();
-       }
-
-       if (val != '8') {
-          strip.setPixelColor(139, 0, 0, 0);
-          strip.show();
-       }
-       if (val == '9') 
-       { // If 1 was received
-          strip.setPixelColor(140, 255, 0, 0);
-          strip.show();
-           
-       } if (val != '9') {
-          strip.setPixelColor(140, 0, 0, 0);
-          strip.show();
-       }
-       
   
 }
 
--- processing1_0/processing1_0.pde
+++ processing1_0/processing1_0.pde
@@ -1,63 +1,22 @@
 import processing.serial.*;
 
-Serial myPort;  // Create object from Serial class
+Serial port;  // Create object from Serial class
 
 void setup() 
 {
-  size(1200,200); //make our canvas 200 x 200 pixels big
+  size(255,255); //make our canvas 200 x 200 pixels big
   String portName = Serial.list()[2]; //change the 0 to a 1 or 2 etc. to match your port
-  myPort = new Serial(this, portName, 9200);
+  port = new Serial(this, portName, 9600);
 }
 
 void draw() {
- if (mouseX < 100){
-     myPort.write('1');         //send a 1
-     println("1");   
- } 
- if (mouseX < 200 && mouseX >=100){
-     myPort.write('2');         //send a 1
-     println("2");   
-}
- if (mouseX < 300 && mouseX >=200){
-     myPort.write('3');         //send a 1
-     println("3");   
-}
- if (mouseX < 400 && mouseX >=300){
-     myPort.write('4');         //send a 1
-     println("4");   
-}
- if (mouseX < 500 && mouseX >=400){
-     myPort.write('5');         //send a 1
-     println("5");   
-}
- if (mouseX < 600 && mouseX >=500){
-     myPort.write('6');         //send a 1
-     println("6");   
-}
- if (mouseX < 700 && mouseX >=600){
-     myPort.write('7');         //send a 1
-     println("7");   
-}
- if (mouseX < 800 && mouseX >=700){
-     myPort.write('8');         //send a 1
-     println("8");   
-}
- if (mouseX < 900 && mouseX >=800){
-     myPort.write('9');         //send a 1
-     println("9");   
-}
 
+int x = mouseX;
+int y = mouseY;
+
+byte out[] = new byte[2];
+out[0] = byte(x);
+out[1] = byte(y);
+
+port.write(out);
 }
-
-
-//void draw() {
-//  if (mousePressed == true) 
-//  {                           //if we clicked in the window
-//   myPort.write('1');         //send a 1
-//   println("1");   
-//  } else 
-//  {                           //otherwise
-//  myPort.write('0');          //send a 0
-//  println("0");
-//  }   
-//
